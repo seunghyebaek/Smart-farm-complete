b40cf94a765c4af7ce9a04e7581b456fee5b332d
Seunghye Baek <38126662+seunghyebaek@users.noreply.github.com>
2025-02-25 05:25:16 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,20 @@
+import os
 from fastapi import FastAPI
+import gradio as gr
+
+# Gradio 페이지 import
+from backend.gradio_english import demo_en
+from backend.gradio_japanese import demo_jp
+from backend.gradio_korean import demo_kr
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI!"}
+    return {"message": "Hello, This is the main app!"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+# Gradio 앱들을 개별 FastAPI 인스턴스로 마운트
+app.mount("/korean", gr.mount_gradio_app(FastAPI(), demo_kr, path=""))
+app.mount("/japanese", gr.mount_gradio_app(FastAPI(), demo_jp, path=""))
+app.mount("/english", gr.mount_gradio_app(FastAPI(), demo_en, path=""))
+
